a3f57a0b3089f5a1ffc84d9d53136f5a7bc625d5
mobizt <k_suwatchai@hotmail.com>
2019-01-28 00:14:17 +0700

    Update FirebaseESP8266HTTPClient.cpp

--- FirebaseESP8266HTTPClient.cpp
+++ FirebaseESP8266HTTPClient.cpp
@@ -1,3 +1,33 @@
+/*
+ * Google's Firebase real time database - ESP8266 library for Arduino, version 1.0
+ * 
+ * This library provides ESP8266 to perform REST API by GET PUT, POST, PATCH, DELETE data from/to with Google's Firebase database using get, set, update
+ * and delete calls. 
+ * 
+ * The library was test and work well with ESP8266s based module and add support for multiple stream event path.
+ * 
+ * The MIT License (MIT)
+ * Copyright (c) 2019 K. Suwatchai (Mobizt)
+ * 
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #ifndef FirebaseESP8266HTTPClient_CPP
 #define FirebaseESP8266HTTPClient_CPP
 
