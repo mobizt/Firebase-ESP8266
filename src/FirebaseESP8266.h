c59575774cd452c5b6665d0cce3bf8da0464e323
suwatchai <k_suwatchai@hotmail.com>
2021-03-30 08:09:25 +0700

    Fix the pushArray, setArray and pathExist bugs.

--- src/FirebaseESP8266.h
+++ src/FirebaseESP8266.h
@@ -1,10 +1,10 @@
 /**
- * Google's Firebase Realtime Database Arduino Library for ESP8266, version 3.1.6
+ * Google's Firebase Realtime Database Arduino Library for ESP8266, version 3.1.7
  * 
- * March 29, 2021
+ * March 30, 2021
  * 
  *   Updates:
- * - Fix the FCM bugs due to porting from the new library.
+ * - Fix the pushArray, setArray and pathExist bugs.
  * 
  * This library provides ESP8266 to perform REST API by GET PUT, POST, PATCH, DELETE data from/to with Google's Firebase database using get, set, update
  * and delete calls. 
