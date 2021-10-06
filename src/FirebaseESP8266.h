2fa1e30ff61e7f908279d8a70ed6513723274503
suwatchai <k_suwatchai@hotmail.com>
2021-10-06 20:06:54 +0700

    Add support user re-authentication.

--- src/FirebaseESP8266.h
+++ src/FirebaseESP8266.h
@@ -1,15 +1,16 @@
 
 #ifndef FIREBASE_CLIENT_VERSION
-#define FIREBASE_CLIENT_VERSION "3.5.3"
+#define FIREBASE_CLIENT_VERSION "3.5.5"
 #endif
 
 /**
- * Google's Firebase Realtime Database Arduino Library for ESP8266, v3.5.4
+ * Google's Firebase Realtime Database Arduino Library for ESP8266, v3.5.5
  * 
- * Created October 3, 2021
+ * Created October 6, 2021
  *
  *   Updates:
- * - Update README and examples.
+ * - Add support user re-authentication.
+ * - Add re-authenticate example.
  * 
  *
  * This library provides ESP8266 to perform REST API by GET PUT, POST, PATCH, DELETE data from/to with Google's Firebase database using get, set, update
