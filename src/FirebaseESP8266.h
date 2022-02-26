fd8427f8ec113d719dc75cca65eedeeef3f1eace
suwatchai <k_suwatchai@hotmail.com>
2022-02-26 21:22:48 +0700

    Fixed compile guard for base client issue.

--- src/FirebaseESP8266.h
+++ src/FirebaseESP8266.h
@@ -1,16 +1,15 @@
 
 #ifndef FIREBASE_CLIENT_VERSION
-#define FIREBASE_CLIENT_VERSION "3.9.0"
+#define FIREBASE_CLIENT_VERSION "3.9.1"
 #endif
 
 /**
- * Google's Firebase Realtime Database Arduino Library for ESP8266, v3.9.0
+ * Google's Firebase Realtime Database Arduino Library for ESP8266, v3.9.1
  *
- * Created February 20, 2022
+ * Created February 26, 2022
  *
  *   Updates:
- * - Fixed exception error due flash string manipulation issue in FirebaseJson.
- * - Fixed stream and queue tasks issue.
+ * - Fixed compile guard for base client issue..
  *
  *
  * This library provides ESP8266 to perform REST API by GET PUT, POST, PATCH, DELETE data from/to with Google's Firebase database using get, set, update
