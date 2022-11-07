81286b64af00b7caca13670099b84eaa752b35ce
unknown <k_suwatchai@hotmail.com>
2022-11-07 10:41:48 +0700

    Fix upload/download progress elapsed time.

--- src/FirebaseESP8266.h
+++ src/FirebaseESP8266.h
@@ -1,16 +1,15 @@
 
 #ifndef FIREBASE_CLIENT_VERSION
-#define FIREBASE_CLIENT_VERSION "4.2.1"
+#define FIREBASE_CLIENT_VERSION "4.2.3"
 #endif
 
 /**
- * Google's Firebase Realtime Database Arduino Library for ESP8266, v4.2.1
+ * Google's Firebase Realtime Database Arduino Library for ESP8266, v4.2.3
  *
- * Created November 4, 2022
+ * Created November 7, 2022
  *
  *   Updates:
- * - Add suuport Arduino StringSumHelper class for Arduino String concatenation used in function arguments and config.
- * - Update external Client examples to use built-in SSL Client.
+ * - Fix upload/download progress elapsed time.
  *
  *
  * This library provides ESP8266 to perform REST API by GET PUT, POST, PATCH, DELETE data from/to with Google's Firebase database using get, set, update
